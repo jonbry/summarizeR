582c06aacb22b14c467795b5846d85fc1d10d3b6
Jon Bryant <jon@jonbryant.co>
2025-09-13 20:40:29 -0400

    Started Data Veiwer App

---
+++ R/app.R
@@ -0,0 +1,34 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Data Viewer"),
+  fluidRow(
+    column(
+      4,
+      fileInput(
+        inputId = "uploadFile",
+        label = "Upload File",
+        buttonLabel = "Upload",
+        width = 300
+      )
+    ),
+    column(
+      6,
+      numericInput(
+        inputId = "quantile",
+        label = "Quantile",
+        value = 25,
+        width = 100
+      )
+    )
+  ),
+  actionButton(
+    inputId = "Submit",
+    label = "Submit"
+  )
+)
+server <- function(input, output) {
+  
+}
+
+shinyApp(ui = ui, server = server)
